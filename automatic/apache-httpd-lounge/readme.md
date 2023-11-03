a227f41111d59d5a000e3ef241bbe5c3ae22b43f
Blake Martin <blake.martin@archerirm.com>
2023-11-03 18:04:44 -0500

    feat: Add apache-httpd-lounge package with download parsing

---
+++ automatic/apache-httpd-lounge/readme.md
@@ -0,0 +1,37 @@
+# <img src="https://cdn.jsdelivr.net/gh/chocolatey-community/chocolatey-packages@2bdf6f7e33ec1a8126829fbbc87b83e4473b3634/icons/apache-httpd.png" width="48" height="48"/> [apache-httpd](https://chocolatey.org/packages/apache-httpd)
+
+Apache HTTP Web Server for Windows.
+
+The Apache HTTP Server Project is an effort to develop and maintain an open-source HTTP server for modern operating systems including UNIX and Windows. The goal of this project is to provide a secure, efficient and extensible server that provides HTTP services in sync with the current HTTP standards.
+
+The Apache HTTP Server ("httpd") was launched in 1995 and it has been the most popular web server on the Internet since April 1996. It has celebrated its 20th birthday as a project in February 2015.
+
+The Apache HTTP Server is a project of The Apache Software Foundation.
+
+## Package Parameters
+
+- `/installLocation` - Install to a different destination folder. Default: `$Env:AppData\Apache*`
+- `/serviceName` - The name of the windows service which will be create. Default: `Apache`
+- `/port` - The port Apache will listen to. Default: `8080`
+- `/noService` - Don't install the apache httpd windows service
+
+Example: `choco install apache-httpd --params '"/installLocation:C:\HTTPD /port:433"'`
+
+## Notes
+
+- This package will install the latest Apache binaries provided at Apache Lounge (https://www.apachelounge.com//) built with the following dependecies as of 2.4.58
+  - openssl 3.1.3
+  - nghttp2 1.57.0
+  - jansson 2.14
+  - curl 8.4.0
+  - apr 1.7.3
+  - apr-util 1.6.3
+  - apr-iconv 1.2.2
+  - zlib 1.2.13
+  - brotli 1.1.0
+  - pcre2 10.42
+  - libxml2 2.11.5
+  - lua 5.4.6
+  - expat 2.5.0
+- The complete path of the package will be `$Env:AppData\Apache*`
+- Apache will be installed as a service under the default name 'Apache' (can be disabled with the `/noService` install parameter)
---
+++ automatic/apache-httpd-lounge/update.ps1
@@ -0,0 +1,53 @@
+﻿Import-Module au
+
+$baseUrl = "https://www.apachelounge.com"
+$mostRecentRelease = "$baseUrl/download/"
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix }
+
+function global:au_GetLatest {
+  $versionRegEx = '(?<=href=").*httpd\-([\d\.]+).*\-win64\-([Vv][Ss]\d{2}).*\.zip(?=")'
+
+  $baseUri = [System.Uri]::new($baseUrl)
+
+  $downloadPage = Invoke-WebRequest -Uri $mostRecentRelease -UseBasicParsing -HttpVersion 2.0 -UserAgent "Chocolatey Auto Update"
+  $match = [regex]::match($downloadPage.Content, $versionRegEx)
+  $version64 = [version]$match.Groups[1].Value
+  $absolutePath64 = $match.Value
+  $uri64 = [System.Uri]::new($baseUri, $absolutePath64)
+
+  $versionRegEx = $versionRegEx -replace 'win64', 'win32'
+  $match = [regex]::match($downloadPage.Content, $versionRegEx)
+  $version32 = [version]$match.Groups[1].Value
+  $absolutePath32 = $match.Value
+  $uri32 = [System.Uri]::new($baseUri, $absolutePath32)
+
+  if ($version32 -ne $version64) {
+    throw "32bit and 64bit version do not match. Please check the update script."
+  }
+
+  return @{
+    Url32   = $uri32.ToString()
+    Url64   = $uri64.ToString()
+    Version = $version64
+  }
+}
+
+function global:au_SearchReplace {
+  return @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+    ".\legal\VERIFICATION.txt"      = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
+      "(?i)(64-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL64)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+      "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
+    }
+  }
+}
+
+update -ChecksumFor None
